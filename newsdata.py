864ff53625a0fb7d632fce959e92b8e93bc06f8f
Jenni Weegar <jenniweegar@gmail.com>
2018-01-12 14:21:46 -0500

    Update newsdata.py

--- newsdata.py
+++ newsdata.py
@@ -4,27 +4,27 @@
 import psycopg2
 
 
-#Q1 What are the most popular three articles of all time?
-question_1_title = ("What are the most popular three articles of all time?")
-question_1 = (
+# What are the most popular three articles of all time?
+Q1_title = ("What are the most popular three articles of all time?")
+Q1 = (
     "select articles.title, count(*) as views "
     "from articles inner join log on log.path "
     "like concat('%', articles.slug, '%') "
     "where log.status like '%200%' group by "
     "articles.title, log.path order by views desc limit 3")
 
-#Q2 Who are the most popular article authors of all time?
-question_2_title = ("Who are the most popular article authors of all time?")
-question_2 = (
+# Who are the most popular article authors of all time?
+Q2_title = ("Who are the most popular article authors of all time?")
+Q2 = (
     "select authors.name, count(*) as views from articles inner "
     "join authors on articles.author = authors.id inner join log "
     "on log.path like concat('%', articles.slug, '%') where "
     "log.status like '%200%' group "
     "by authors.name order by views desc")
 
-#Q3 On which days did more than 1% of requests lead to errors
-question_3_title = ("On which days did more than 1% of requests lead to errors?")
-question_3 = (
+# On which days did more than 1% of requests lead to errors
+Q3_title = ("On which days did more than 1% of requests lead to errors?")
+Q3 = (
     "select day, perc from ("
     "select day, round((sum(requests)/(select count(*) from log where "
     "substring(cast(log.time as text), 0, 11) = day) * 100), 2) as "
@@ -53,6 +53,7 @@ def get_query_results(query):
 
 
 def print_query_results(query_results):
+    """Format how Q1 & Q2 results will display"""
     print (query_results[1])
     for index, results in enumerate(query_results[0]):
         print (
@@ -61,6 +62,7 @@ def print_query_results(query_results):
 
 
 def print_error_results(query_results):
+    """Format how Q3 results display"""
     print (query_results[1])
     for results in query_results[0]:
         print ("\t", results[0], ' ', str(results[1]) + "% errors")
@@ -68,11 +70,11 @@ def print_error_results(query_results):
 
 if __name__ == '__main__':
     # Storing query results
-    popular_articles_results = get_query_results(question_1), question_1_title
-    popular_authors_results = get_query_results(question_2), question_2_title
-    load_error_days = get_query_results(question_3), question_3_title
+    articles_results = get_query_results(Q1), Q1_title
+    authors_results = get_query_results(Q2), Q2_title
+    error_days = get_query_results(Q3), Q3_title
 
     # Printing query results
-    print_query_results(popular_articles_results)
-    print_query_results(popular_authors_results)
-    print_error_results(load_error_days)
+    print_query_results(articles_results)
+    print_query_results(authors_results)
+    print_error_results(error_days)
